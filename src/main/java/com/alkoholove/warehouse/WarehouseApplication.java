af0a4b52f00e7d051e8a34ec99c887bb902420b7
adam-przybylski <adamprzybylski789@gmail.com>
2024-02-06 16:38:20 +0100

    dotenv load

--- src/main/java/com/alkoholove/warehouse/WarehouseApplication.java
+++ src/main/java/com/alkoholove/warehouse/WarehouseApplication.java
@@ -1,5 +1,6 @@
 package com.alkoholove.warehouse;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WarehouseApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load();
+
+        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(WarehouseApplication.class, args);
     }
 
