a6494b6899d54b6e592fea4066fc7b724b3564d0
adam-przybylski <adamprzybylski789@gmail.com>
2024-02-23 16:30:52 +0100

    types fix

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,9 +1,9 @@
-import {apiWithConfig} from "./api.config.ts";
-import {ApiResponseType} from "../types/ApiResponseType.ts";
-import {AccountType} from "../types/AccountType.ts";
-import {ClientType} from "../types/ClientType.ts";
-import {ProductType} from "../types/ProductType.ts";
-import {ReservationGet, ReservationPost} from "../types/ReservationType.ts";
+import { apiWithConfig } from "./api.config.ts";
+import { ApiResponseType } from "../types/ApiResponseType.ts";
+import { AccountType } from "../types/AccountType.ts";
+import { ClientType } from "../types/ClientType.ts";
+import { ProductType } from "../types/ProductType.ts";
+import { ReservationGet, ReservationPost } from "../types/ReservationType.ts";
 
 
 export const api = {
@@ -17,45 +17,45 @@ export const api = {
     getAccounts: (): ApiResponseType<AccountType[]> => {
         return apiWithConfig.get(`/accounts`)
     },
-    enableAccount: (username: string): ApiResponseType<AccountType> => {
+    enableAccount: (username: string | undefined): ApiResponseType<AccountType> => {
         return apiWithConfig.patch(`/accounts/enable/${username}`)
     },
-    disableAccount: (username: string): ApiResponseType<AccountType> => {
+    disableAccount: (username: string | undefined): ApiResponseType<AccountType> => {
         return apiWithConfig.patch(`/accounts/disable/${username}`)
     },
     updatePassword: (username: string | undefined, password: string | undefined): ApiResponseType<AccountType> => {
         return apiWithConfig.put(`/accounts/${username}`, { username, password })
     },
     addAccount: (account: AccountType): ApiResponseType<AccountType> => {
-        return apiWithConfig.post(`/accounts`, {...account})
+        return apiWithConfig.post(`/accounts`, { ...account })
     },
 
-    getClients:(): ApiResponseType<ClientType[]> => {
+    getClients: (): ApiResponseType<ClientType[]> => {
         return apiWithConfig.get(`/clients`)
     },
     getClient: (username: string): ApiResponseType<ClientType> => {
         return apiWithConfig.get(`/clients/${username}`)
     },
     addClient: (client: ClientType): ApiResponseType<ClientType> => {
-        return apiWithConfig.post(`/clients`, {...client})
+        return apiWithConfig.post(`/clients`, { ...client })
     },
-    deleteClient: (username: string): ApiResponseType<ClientType> => {
+    deleteClient: (username: string | undefined): ApiResponseType<ClientType> => {
         return apiWithConfig.delete(`/clients/${username}`)
     },
 
     getProducts: (): ApiResponseType<ProductType[]> => {
         return apiWithConfig.get(`/products`)
     },
-    updateNumberOfProductItems: (product: ProductType):  ApiResponseType<ProductType> => {
-        return apiWithConfig.put(`/products/${product.name}`, {...product})
+    updateNumberOfProductItems: (product: ProductType): ApiResponseType<ProductType> => {
+        return apiWithConfig.put(`/products/${product.name}`, { ...product })
     },
-    updateProducts: (products: ProductType[]):  ApiResponseType<ProductType[]> => {
+    updateProducts: (products: ProductType[]): ApiResponseType<ProductType[]> => {
         return apiWithConfig.put(`/products`, products)
     },
-    addProduct: (product: ProductType):  ApiResponseType<ProductType> => {
-        return apiWithConfig.post(`/products`, {...product})
+    addProduct: (product: ProductType): ApiResponseType<ProductType> => {
+        return apiWithConfig.post(`/products`, { ...product })
     },
-    deleteProduct: (name: string):  ApiResponseType<ProductType> => {
+    deleteProduct: (name: string): ApiResponseType<ProductType> => {
         return apiWithConfig.delete(`/products/${name}`)
     },
 
@@ -69,7 +69,7 @@ export const api = {
         return apiWithConfig.get(`/reservations/status/delivered`)
     },
     addReservation: (reservation: ReservationPost): ApiResponseType<ReservationGet> => {
-        return apiWithConfig.post(`/reservations`, {...reservation})
+        return apiWithConfig.post(`/reservations`, { ...reservation })
     },
     updateReservationStatus: (id: string): ApiResponseType<ReservationGet> => {
         return apiWithConfig.patch(`/reservations/${id}`)
